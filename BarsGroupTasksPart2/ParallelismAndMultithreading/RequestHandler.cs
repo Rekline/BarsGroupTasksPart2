31ad93beec17af2f88c4733d2f13e59b97626b00
Rekline <vladiva312003@gmail.com>
2022-04-09 18:43:57 +0300

    Little fix

--- BarsGroupTasksPart2/ParallelismAndMultithreading/RequestHandler.cs
+++ BarsGroupTasksPart2/ParallelismAndMultithreading/RequestHandler.cs
@@ -12,21 +12,25 @@ namespace BarsGroupTasksPart2
         {
             var arguments = new List<string>();
             Console.WriteLine("Приложение запущено;");
+
             do
             {
                 Console.WriteLine("Введите текст запроса для отправки. Для выхода введите /exit.");
                 var taskText = Console.ReadLine();
+
                 while (taskText == "")
                 {
                     Console.WriteLine("Введён пустой текст запроса. Исправьте текст и повторите попытку.");
                     taskText = Console.ReadLine();
                 }
+
                 if (taskText == "/exit")
                     break;
                 Console.WriteLine($"Будет послано сообщение '{taskText}'");
 
                 Console.WriteLine("Введите аргументы сообщения. Для окончания добавления аргументов введите /end");
                 var taskArg = Console.ReadLine();
+
                 while (taskArg != "/end")
                 {
                     while (taskArg == "")
@@ -38,6 +42,7 @@ namespace BarsGroupTasksPart2
                     Console.WriteLine("Введите следующий аргумент сообщения. Для окончания добавления аргументов введите /end");
                     taskArg = Console.ReadLine();
                 }
+
                 if (taskArg == "/end")
                 {
                     var requestId = Guid.NewGuid().ToString("D");
@@ -56,7 +61,6 @@ namespace BarsGroupTasksPart2
                         }
                     });
                 }
-                
             } while (true);
             Console.WriteLine("Приложение завершает работу");
         }
